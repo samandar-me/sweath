f949c0993a6a18451b49de670a815a4067a78a84
samandar-mee <samandarcoderdev@gmail.com>
2022-12-23 22:33:24 +0500

    Initial Commit

---
+++ iosmvvm/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iosmvvm
+//
+//  Created by Macbook on 29.05.1444 (AH).
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iosmvvm/iosmvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosmvvmApp.swift
+//  iosmvvm
+//
+//  Created by Macbook on 29.05.1444 (AH).
+//
+
+import SwiftUI
+
+@main
+struct iosmvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
